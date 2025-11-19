3dcb99a5f1d98484a3633faf7d6686e970e5a5c6
Alexandre da Silva Popov <alexandre.popov406@academico.ufgd.edu.br>
2025-11-19 00:47:36 -0400

    Arrumando botões no mobile

--- componentes/hero.php
+++ componentes/hero.php
@@ -4,21 +4,25 @@
         <h1 class="text-3xl font-bold">Desenvolvedor Back-end (Estudante)</h1>
         <p class="text-xl leading-6 mt-6">
             Estudante do 4º semestre de Engenharia de Computação na UFGD, com forte base em desenvolvimento de software
-             e proficiência em C++, Python e PHP. Busco uma oportunidade para aplicar minhas habilidades técnicas na resolução 
-             de problemas complexos e na criação de soluções back-end eficientes e escaláveis.
+            e proficiência em C++, Python e PHP. Busco uma oportunidade para aplicar minhas habilidades técnicas na resolução
+            de problemas complexos e na criação de soluções back-end eficientes e escaláveis.
         </p>
-        <a
-            href="mailto:alexandrepopov4@gmail.com?subject=Olá, vi seu portfólio e gostaria de conversar sobre um projeto."
-            target="_blank"
-            class="mt-6 inline-block bg-white text-black font-semibold px-6 py-3 rounded-lg shadow-xl hover:bg-gray-200 transition duration-300 transform hover:scale-105">
-            Enviar Email 📧
-        </a>
-        <a
-            href="https://wa.me/5511963826052/?text=Olá, vi seu portfólio e gostaria de conversar sobre um projeto."
-            target="_blank"
-            class="mt-6 inline-block bg-white text-black font-semibold px-6 py-3 rounded-lg shadow-xl hover:bg-gray-200 transition duration-300 transform hover:scale-105">
-            Conversar no WhatsApp 💬
-        </a>
+
+        <div class="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-6 sm:space-y-0 mt-8"> 
+            <a
+                href="mailto:alexandrepopov4@gmail.com?subject=Olá, vi seu portfólio e gostaria de conversar sobre um projeto."
+                target="_blank"
+                class="w-full max-w-sm sm:flex-1 inline-block bg-white text-black font-semibold px-6 py-4 rounded-lg shadow-lg hover:bg-gray-200 transition duration-300 transform hover:scale-105 text-center whitespace-nowrap">
+                Enviar Email 📧
+            </a>
+            <a
+                href="https://wa.me/5511963826052/?text=Olá, vi seu portfólio e gostaria de conversar sobre um projeto."
+                target="_blank"
+                class="w-full max-w-sm sm:flex-1 inline-block bg-white text-black font-semibold px-6 py-4 rounded-lg shadow-lg hover:bg-gray-200 transition duration-300 transform hover:scale-105 text-center whitespace-nowrap">
+                Conversar no WhatsApp 💬
+            </a>
+        </div>
+
     </div>
 
     <!-- Imagem -->
